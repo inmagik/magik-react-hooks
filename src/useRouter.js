5d4d05c997c360a6fc7e658d3b4bdbf0763aa0d4
gffuma <fumagalli.gf@gmail.com>
2020-09-03 15:51:32 +0200

    Improve useRouter deprecation warning

--- src/useRouter.js
+++ src/useRouter.js
@@ -1,15 +1,16 @@
 import { useLocation, useHistory } from 'react-router'
 
+if (process.env.NODE_ENV !== 'production') {
+  console.warn(
+    'useRouter is depreacted and will be removed in the next major release, ' +
+    'use native react-router hooks instead:\n' +
+      "import { useLocation, useHistory } from 'react-router'"
+  )
+}
+
 export default function useRouter() {
   const location = useLocation()
   const history = useHistory()
-
-  if (process.env.NODE_ENV !== 'production') {
-    console.warn(
-      'useRouter is depreacted and will be removed in the next major release ' +
-        "plase use: import { useLocation, useHistory } from 'react-router' instead. "
-    )
-  }
 
   return { location, history }
 }
