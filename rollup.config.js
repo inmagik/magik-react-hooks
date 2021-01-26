113ecbdb4505df5b31a6fe694778438eadeaa128
gffuma <fumagalli.gf@gmail.com>
2021-01-26 15:15:13 +0100

    Actualize build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
 import fs from 'fs'
+import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
 import pkg from './package.json'
-import babel from 'rollup-plugin-babel'
+
+const extensions = ['.ts', '.tsx', '.js', '.jsx']
 
 const hooks = fs
   .readdirSync('src')
   .filter(item => {
-    return item[0] !== '.' && item.match(/^(use)[\w]+(\.)(js)$/i) !== null
+    return item[0] !== '.' && item.match(/^(use)[\w]+(\.)(ts)$/i) !== null
   })
 
 const stripExtension = file => file.replace(/\.[^.]+$/, '')
@@ -17,22 +20,49 @@ const vendors = []
     Object.keys(pkg.peerDependencies || []),
   )
 
+const makeExternalPredicate = (externalArr) => {
+  if (externalArr.length === 0) {
+    return () => false
+  }
+  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`)
+  return (id) => pattern.test(id)
+}
+
 export default ['esm', 'cjs'].map(format => ({
   input: {
     ...hooks.reduce((all, hook) => ({
       ...all,
       [stripExtension(hook)]: 'src/' + hook
     }), {}),
-    'qpUtils': 'src/qpUtils.js'
+    'qpUtils': 'src/qpUtils.ts'
   },
   output: [
     {
       dir: 'lib',
       entryFileNames: '[name].[format].js',
+      chunkFileNames: '[name].[format].js',
       exports: 'named',
       format
     }
   ],
-  external: vendors,
-  plugins: [babel({ exclude: 'node_modules/**' })]
+  external: makeExternalPredicate(vendors),
+  plugins: [
+    resolve({ extensions }),
+    babel({
+      babelHelpers: 'runtime',
+      exclude: 'node_modules/**',
+      extensions,
+      plugins: [
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            useESModules: format === 'esm',
+            // NOTE: Sometimes js world is a pain
+            // see: https://github.com/babel/babel/issues/10261
+            version: pkg['dependencies']['@babel/runtime'],
+          },
+        ],
+      ],
+    }),
+  ]
 }))
--- scripts/makeDefs.js
+++ scripts/makeDefs.js
@@ -5,7 +5,7 @@ const stripExtension = file => file.replace(/\.[^.]+$/, '')
 const hooks = fs
   .readdirSync('src')
   .filter(item => {
-    return item[0] !== '.' && item.match(/^(use)[\w]+(\.)(js)$/i) !== null
+    return item[0] !== '.' && item.match(/^(use)[\w]+(\.)(ts)$/i) !== null
   })
   .map(stripExtension)
 
@@ -18,7 +18,8 @@ hooks.forEach(hook => {
     name: 'magik-react-hooks/' + hook,
     private: true,
     main: '../lib/' + hook + '.cjs.js',
-    module: '../lib/' + hook + '.esm.js'
+    module: '../lib/' + hook + '.es.js',
+    types: '../lib/' + hook + '.d.ts'
   }, null, 2))
 })
 
@@ -27,5 +28,6 @@ fs.writeFileSync('qpUtils/package.json', JSON.stringify({
   name: 'magik-react-hooks/qpUtils',
   private: true,
   main: '../lib/qpUtils.cjs.js',
-  module: '../lib/qpUtils.esm.js'
+  module: '../lib/qpUtils.es.js',
+  types: '../lib/qpUtils.d.ts'
 }, null, 2))
